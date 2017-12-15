d3ded940e9f91041ab58f9b0c79f582a3cbe4bf2
motorin <pavel.motorin@gmail.com>
2017-12-15 15:55:51 +0300

    redesign routes -> move logic to controllers

---
+++ src/controllers/twitter.js
@@ -0,0 +1,5 @@
+
+export default async function (req, res) {
+  const { hours } = req.query;
+  // get from db feed for last hours
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,23 @@
-import twitter from './twitter';
-import bot from './bot';
+import Express from 'express';
+import wrap from 'express-async-wrap';
+
+import botStopLossUp from '../controllers/bot-stoploss-up/';
+import profitCheck from '../controllers/profit_check';
+import twitter from '../controllers/twitter';
+
+const Router = new Express.Router();
 
 export default [
-  twitter,
-  bot,
+  // Twitter
+  Router.get('/api/twitter', wrap(twitter)),
+
+  // Check Your Own Profit
+  Router.get('/api/profit_check', wrap(profitCheck)),
+
+  // Bot For Settings StopLoss higher in auto mode
+  Router.get('/api/bot/high', wrap(botStopLossUp)),
+
+  // Bot For re-buy at the same price after sell by stop-loss order
+  // Router.get('/api/bot/rebuy', wrap(botStopLossUp)),
+
 ];
