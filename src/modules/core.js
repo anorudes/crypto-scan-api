a26f92e83dabbe3394cb4b705871ed582d8b9f72
anorudes <anorudes@gmail.com>
2017-12-16 12:41:44 +0300

    Commented Core

--- src/modules/core.js
+++ src/modules/core.js
@@ -4,23 +4,23 @@ export class Core {
   }
 
   init() {
-    setInterval(() => {
-      this.twitterParser.parseHomeFeed(data => {
-        // check with last result
-        // save to db new data
-        const tweets = [];
-        data.map(tweet => {
-          const date = new Date(tweet.created_at);
-
-          tweets.push({
-            date,
-            text: tweet.text,
-          });
-        });
-
-        global.tweets = tweets;
-      });
-    }, 1000 * 60 * 20);
+    // setInterval(() => {
+    //   this.twitterParser.parseHomeFeed(data => {
+    //     // check with last result
+    //     // save to db new data
+    //     const tweets = [];
+    //     data.map(tweet => {
+    //       const date = new Date(tweet.created_at);
+//
+    //       tweets.push({
+              //         date,
+            //         text: tweet.text,
+    //       });
+    //     });
+//
+    //     global.tweets = tweets;
+    //   });
+    // }, 1000 * 60 * 20);
   }
 }
 
