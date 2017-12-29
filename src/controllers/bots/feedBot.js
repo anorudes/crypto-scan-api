545b71220d64cc74c09d56d11bde62ab5a86279b
anorudes <anorudes@gmail.com>
2017-12-29 22:21:30 +0300

    FIX

--- src/controllers/bots/feedBot.js
+++ src/controllers/bots/feedBot.js
@@ -17,7 +17,7 @@ class FeedBot {
     this.notifyInterval = setInterval(this.notifyFromQueue.bind(this), 1000);
     this.notifyList = [];
     setInterval(this.startParse.bind(this), 3000000);
-    
+
     this.startParse();
   }
 
